dc7b363f91b55a87b5a29d332b76cae81d014661
imtalhaozer <imtalhaozer@gmail.com>
2024-09-01 22:30:25 +0300

    routing changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { BrowserRouter, Routes,Route } from 'react-router-dom'
-import {SideBar} from './components/SideBar'
-import { Aim } from './pages/Aim'
+import { Goals } from './pages/Goals'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<SideBar/>} />
-        <Route path='/hedef' element={<Aim/>}/>
+      <Route path='/' element={<Goals/>}/>
       </Routes>
     </BrowserRouter>
   )
